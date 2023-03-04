e5bd54f93221b6dcc21c5e2e21bda2a26c147d09
ttrocino <tonytrocino@gmail.com>
2023-03-04 17:12:07 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+console.log("hello world");
